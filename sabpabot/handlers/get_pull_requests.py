1e1f2a855ca6d788005565d2c12fa59322b31b00
Zeynab Amini <zeynab0amini@gmail.com>
2024-10-13 19:45:53 +0330

    fix(prs): split long message in to sub parts

--- sabpabot/handlers/get_pull_requests.py
+++ sabpabot/handlers/get_pull_requests.py
@@ -4,6 +4,8 @@ from telegram.ext import ContextTypes
 
 from sabpabot.controllers.pull_request_controller import PullRequestController
 
+MAX_MESSAGE_SIZE = 4096
+
 
 async def prs(update: Update, context: ContextTypes.DEFAULT_TYPE):
     input_message = update.message.text
@@ -13,4 +15,8 @@ async def prs(update: Update, context: ContextTypes.DEFAULT_TYPE):
     sender = sender if sender.startswith('@') else f'@{sender}'
     result = PullRequestController.get_prs(text, group_name, sender)
 
+    while len(result) > MAX_MESSAGE_SIZE:
+        sub_result = result[:MAX_MESSAGE_SIZE].rsplit('\n', 1)[0]
+        await update.message.reply_text(sub_result, parse_mode=ParseMode.MARKDOWN)
+        result = result[len(sub_result) + 1:]
     await update.message.reply_text(result, parse_mode=ParseMode.MARKDOWN)
