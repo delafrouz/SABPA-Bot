fa1e9b5ea401bd527264b64a0b8dfef83bc78ed8
delafrouz <del.mirfendereski@gmail.com>
2024-10-28 00:09:21 +0330

    Fix mongo connection configs

--- sabpabot/mongo_access.py
+++ sabpabot/mongo_access.py
@@ -5,9 +5,10 @@ from pymongo import MongoClient
 
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_CONNECTION_NAME = os.getenv('DB_CONNECTION_NAME')
 DB_NAME = os.getenv('DB_NAME')
 DB_HOST = os.getenv('DB_HOST')
-DB_URL = f'mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_NAME}.{DB_HOST}.mongodb.net/'
+DB_URL = f'mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_CONNECTION_NAME}.{DB_HOST}.mongodb.net/'
 
 client = MongoClient(DB_URL)
 mongo_db = client[DB_NAME]
